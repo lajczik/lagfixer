559dd4fc5cf73115924d60b1ed04a0a70832ae90
lajczi <lajczi@gmail.com>
2026-05-29 12:28:20 +0200

    Bump version to 1.6.1
    Took 5 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val sonatypeRepo = "https://oss.sonatype.org/content/groups/public/"
 val jitpack = "https://jitpack.io"
 val mojang = "https://libraries.minecraft.net"
 
-version = "1.6.0"
+version = "1.6.1"
 extra["lagfixer_version"] = version
 extra["lagfixer_build"] = "143"
 
