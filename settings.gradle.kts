ba82528fce71217d8fa3182be1b90cc2863bfea8
lajczi <lajczi@gmail.com>
2026-06-22 03:13:30 +0200

    Removed unnecessary plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,6 @@ pluginManagement {
         maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 
 rootProject.name = "LagFixer"
 
