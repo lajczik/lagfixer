fdaf59996dfaa5b19d5ef86731fb0dbead990a3a
lajczi <lajczi@gmail.com>
2026-06-25 10:05:59 +0200

    Added nms support for 26.2

---
+++ nms/v26_2/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java")
+    id("io.papermc.paperweight.userdev")
+}
+
+paperweight {
+    javaLauncher = javaToolchains.launcherFor {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+dependencies {
+    paperweight.paperDevBundle("26.2.build.+")
+    compileOnly(project(":plugin"))
+    compileOnly(project(":support:common"))
+}
