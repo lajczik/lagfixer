d1b1c799070eac7048c709bbf1d0bd30541b782a
lajczi <lajczi@gmail.com>
2026-06-25 18:27:02 +0200

    Version bump to 1.6.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ val sonatypeRepo = "https://oss.sonatype.org/content/groups/public/"
 val jitpack = "https://jitpack.io"
 val mojang = "https://libraries.minecraft.net"
 
-version = "1.6.3"
+version = "1.6.4"
 extra["lagfixer_version"] = version
-extra["lagfixer_build"] = "145"
+extra["lagfixer_build"] = "147"
 
 dependencies {
     implementation(project(":plugin"))
