965e3c2ad4f0f600958a34741add866505f12cfa
lajczi <lajczi@gmail.com>
2026-08-16 12:53:01 +0200

    Update gradleup shadow to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "9.4.1"
+    id("com.gradleup.shadow") version "9.6.1"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.21" apply false
 }
 
